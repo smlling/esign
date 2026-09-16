694028dcc1c8c5dc3cb2b54918e94f56f57d397b
smlling <smlling@hotmail.com>
2026-09-16 09:56:35 +0800

    Initial Commit

---
+++ esign/esignApp.swift
@@ -0,0 +1,17 @@
+//
+//  esignApp.swift
+//  esign
+//
+//  Created by smlling on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct esignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
